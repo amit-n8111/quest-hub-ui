e34dda4b042172f696db78ba1aadec88bd5782fb
Amit Navarkar <amitnavarkar111@gmail.com>
2019-07-25 11:36:11 +0530

    Profile detail component.

--- src/app/features/profiles/profiles-routing.module.ts
+++ src/app/features/profiles/profiles-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
       {
         path: '',
         component: ProfileListComponent,
+      },
+      {
+        path: ':id',
+        component: ProfileDetailsComponent
       }
     ]
   }
--- src/app/features/profiles/profiles.module.ts
+++ src/app/features/profiles/profiles.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ProfilesRoutingModule } from './profiles-routing.module';
 import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
 
 
 @NgModule({
-  declarations: [ProfileListComponent],
+  declarations: [ProfileListComponent, ProfileDetailsComponent],
   imports: [
     CommonModule,
     ProfilesRoutingModule
