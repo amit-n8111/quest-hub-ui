51633771a53f22852740c1178f9d84bbfd23572b
Amit Navarkar <amitnavarkar111@gmail.com>
2019-08-16 18:48:55 +0530

    Updating base_url.

--- src/app/features/posts/constants/posts.constants.ts
+++ src/app/features/posts/constants/posts.constants.ts
@@ -1,9 +1,9 @@
 export abstract class PostsConstants {
-    public static TASK_LIST = 'v1/task/tasks';
+    public static TASK_LIST = 'task/tasks';
 
     public static TASK_DETAILS = 'tasks/';
 
-    public static TASK_POST = 'tasks';
+    public static TASK_POST = 'tasks';  // Save Task Details
 
     public static TASK_SUBMIT_APPLICATION = 'task/apply/';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://localhost:8080/api/',
+  BASE_URL: 'http://localhost:8080/api/v1/',
   ASSETS_URL: '/',
   SOCKET_URL: 'http://localhost:3000'
 };
